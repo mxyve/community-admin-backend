2edf1bf018b256a29d1ee45fec86e84a1275cf84
缪心雨 <1286280961@qq.com>
2026-03-01 11:53:46 +0800

    feat：菜单管理模块接口开发

---
+++ base-web/src/main/java/top/xym/web/sys_menu/entity/SysMenu.java
@@ -0,0 +1,38 @@
+package top.xym.web.sys_menu.entity;
+
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@TableName("sys_menu")
+public class SysMenu {
+    @TableId(type = IdType.AUTO)
+    private Long menuId;
+    private Long parentId;
+    private String title;
+    private String code;
+    private String name;
+    private String path;
+    private String url;
+    private String type;
+    private String icon;
+    private String parentName;
+    private Integer orderNum;
+    // 下级菜单 children 字段不属于 sys_menu表，需要排除
+    @TableField(exist = false)
+    private List<SysMenu> children = new ArrayList<>();
+    @TableField(exist = false)
+    private Long value;
+    @TableField(exist = false)
+    private String label;
+    @TableLogic
+    private Integer deleted;
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updateTime;
+}
