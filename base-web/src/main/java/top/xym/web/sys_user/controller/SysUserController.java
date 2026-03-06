c0093152f55c0f912fb574c9da60325665186fbd
缪心雨 <1286280961@qq.com>
2026-03-06 16:26:01 +0800

    feat：获取用户信息接口开发

--- base-web/src/main/java/top/xym/web/sys_user/controller/SysUserController.java
+++ base-web/src/main/java/top/xym/web/sys_user/controller/SysUserController.java
@@ -17,6 +17,8 @@ import top.xym.result.ResultVo;
 import top.xym.utils.ResultUtils;
 import top.xym.web.sys_menu.entity.AssignTreeParm;
 import top.xym.web.sys_menu.entity.AssignTreeVo;
+import top.xym.web.sys_menu.entity.SysMenu;
+import top.xym.web.sys_menu.service.SysMenuService;
 import top.xym.web.sys_user.entity.*;
 import top.xym.web.sys_user.service.SysUserService;
 import top.xym.web.sys_user_role.entity.SysUserRole;
@@ -39,6 +41,7 @@ public class SysUserController {
     private final SysUserRoleService sysUserRoleService;
     private final DefaultKaptcha defaultKaptcha;
     private final JwtUtils jwtUtils;
+    private final SysMenuService sysMenuService;
 
     //新增
     @PostMapping
@@ -209,4 +212,33 @@ public class SysUserController {
         return ResultUtils.error("密码修改失败！");
 
     }
+
+    // 获取用户信息
+    @GetMapping("/getInfo")
+    @Operation(summary = "获取用户信息")
+    public ResultVo<?> getInfo(Long userId) {
+        // 根据id查询用户信息
+        SysUser user = sysUserService.getById(userId);
+        List<SysMenu> menuList;
+        // 判断是否是超级管理员
+        if (StringUtils.isNotEmpty(user.getIsAdmin()) && "1".equals(user.getIsAdmin())) {
+            // 超级管理员，直接全部查询
+            menuList = sysMenuService.list();
+        } else {
+            menuList = sysMenuService.getMenuByUserId(user.getUserId());
+        }
+        // 获取菜单表的code字段
+        List<String> collect = Optional.ofNullable(menuList).orElse(new ArrayList<>())
+                .stream()
+                .filter(item -> item != null && StringUtils.isNotEmpty(item.getCode()))
+                .map(SysMenu::getCode)
+                .toList();
+        // 设置返回值
+        UserInfo userInfo = new UserInfo();
+        userInfo.setName(user.getNickName());
+        userInfo.setUserId(user.getUserId());
+        userInfo.setPermissions(collect.toArray());
+        return ResultUtils.success("查询成功", userInfo);
+    }
+
 }
